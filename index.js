57d909e4b6ddfa303032f1e8ace1a64a6b5938b4
Younes <younesdjbl6@gmail.com>
2022-10-25 18:49:05 +0200

    Tests

---
+++ index.js
@@ -0,0 +1,6 @@
+const co2 = require("./public/co2.json");
+// console.log(co2);
+const doc = require("./public/documentation.json");
+// console.log(doc);
+const personas = require("./public/personas.json");
+console.log(personas);
